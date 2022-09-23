f99281b2016b03f336dc504dede0e2acce534b49
faircm <91977339+faircm@users.noreply.github.com>
2022-09-22 18:42:22 -0700

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TravelRecordApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        public App(string databaseLocation)
+        {
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Clicked(object sender, EventArgs e)
+        {
+            if (emailEntry.Text.Length != 0 || passwordEntry.Text.Length != 0)
+            {
+                if (passwordEntry.Text.Equals("password"))
+                {
+                    // Open next page
+                    Navigation.PushAsync(new HomePage());
+                }
+            }
+            else
+            {
+                // Pop error message
+            }
+        }
+    }
+}
